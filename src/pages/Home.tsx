98342b3c4d101ad2bed45c421caca30e02f2bb8a
vanshita shah <vanshita.s@simformsolutions.com>
2023-12-13 14:06:44 +0530

    feat: add responsive hero section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
+import { Button } from "antd";
+import ArrowDown from "../components/hero-section/ArrowDown";
+import HeroContent from "../components/hero-section/HeroContent";
+import HeroImg from "../components/hero-section/HeroImg";
 import Layout from "../layout/Layout";
 
 const Home = () => {
   return (
     <>
       <Layout>
-        <div>Home</div>
+        <section className="container flex section-hero">
+          <HeroContent />
+          <HeroImg />
+        </section>
+        <ArrowDown />
       </Layout>
     </>
   );
--- src/theme/Theme.ts
+++ src/theme/Theme.ts
@@ -1,9 +1,14 @@
 export const theme = {
+  token: {
+    colorPrimary: "#ff592c",
+  },
   components: {
     Button: {
       borderRadius: 90,
       fontSize: 12,
       fontWeight: 800,
+      paddingBlock: "20px",
+      paddingInline: "28px",
     },
   },
 };
