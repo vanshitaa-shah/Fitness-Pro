d8ef9d91a431ed6e9909cd5dc9d28600d9ea15a7
vanshita shah <vanshita.s@simformsolutions.com>
2023-12-22 17:11:15 +0530

    fix: fix cursors,and add error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,18 @@
+import { Button } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="error-page">
+      <h1>404 Page Not Found!!</h1>
+      <p> The requested page could not be found.</p>
+
+      <Button type="primary">
+        <Link to="/">Home</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default Error;
